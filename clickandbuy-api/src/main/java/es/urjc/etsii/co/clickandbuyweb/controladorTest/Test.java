d18ada9582e7b2d40b872687e13381540e26f3c5
Alvaro Martinez <alvaroo2302@gmail.com>
2021-02-15 13:12:22 +0100

    tests

--- clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/controladorTest/Test.java
+++ clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/controladorTest/Test.java
@@ -3,13 +3,26 @@ package es.urjc.etsii.co.clickandbuyweb.controladorTest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class Test {
 	
-	@GetMapping(value="/test")
+	/*
+	 * Logger class useful methods:
+	 * -> .trace
+	 * -> .debug
+	 * -> .info
+	 * -> .warn
+	 * -> .error
+	 */
+	Logger logger = LoggerFactory.getLogger(Test.class);
+	
+	@GetMapping(value="/hello")
 	public String testMethod() {
-		return "test.html";
+		logger.info("Hello endpoint working fine...");
+		return "hello.html";
 	}
 	
 	@GetMapping(value="/test-2")
