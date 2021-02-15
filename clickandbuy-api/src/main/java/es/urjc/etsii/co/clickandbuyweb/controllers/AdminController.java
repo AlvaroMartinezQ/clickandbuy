f4b50ca72052266b6fb078b8cd4479eccae36542
patri-create <59368869+patri-create@users.noreply.github.com>
2021-02-15 13:43:52 +0100

    feature: admin controller

---
+++ clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/controllers/AdminController.java
@@ -0,0 +1,24 @@
+package es.urjc.etsii.co.clickandbuyweb.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import es.urjc.etsii.co.clickandbuyweb.models.Administrator;
+import es.urjc.etsii.co.clickandbuyweb.service.AdministratorService;
+
+@RestController
+@RequestMapping("/administrators")
+public class AdminController {
+	
+	@Autowired
+	private AdministratorService adminservice;
+	
+	@GetMapping("/all")
+	public List<Administrator> getAdministrator(){
+		return adminservice.getAdministrator();
+	}
+
+}
