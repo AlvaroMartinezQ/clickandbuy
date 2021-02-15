3b5bea6df8f832e86907a2ea6c58dd0c2ca38d10
patri-create <59368869+patri-create@users.noreply.github.com>
2021-02-15 13:45:09 +0100

    feature: administrator service

---
+++ clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/service/AdministratorService.java
@@ -0,0 +1,21 @@
+package es.urjc.etsii.co.clickandbuyweb.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import es.urjc.etsii.co.clickandbuyweb.dao.AdminDAO;
+import es.urjc.etsii.co.clickandbuyweb.models.Administrator;
+
+
+@Service
+public class AdministratorService {
+	
+	@Autowired
+	private AdminDAO admindao;
+	
+	public List<Administrator> getAdministrator(){
+		return admindao.findAll();
+	}
+
+}
