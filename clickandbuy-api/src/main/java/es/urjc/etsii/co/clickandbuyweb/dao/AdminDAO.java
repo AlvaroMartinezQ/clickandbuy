55c35cafeac633e9cd479fd3f4204bb72df715f4
patri-create <59368869+patri-create@users.noreply.github.com>
2021-02-23 17:35:31 +0100

    feature: query admin DAO

--- clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/dao/AdminDAO.java
+++ clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/dao/AdminDAO.java
@@ -1,10 +1,13 @@
 package es.urjc.etsii.co.clickandbuyweb.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import es.urjc.etsii.co.clickandbuyweb.models.Administrator;
 
 @Repository
 public interface AdminDAO extends JpaRepository<Administrator, Integer>{
 
+	@Query("SELECT admin FROM Administrator admin WHERE admin.admin_email=:email")
+	public Administrator findByAdmin_email(String email);
 }
