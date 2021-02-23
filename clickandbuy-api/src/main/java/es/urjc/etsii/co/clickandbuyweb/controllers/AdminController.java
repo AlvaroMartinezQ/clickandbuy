8c2d9cba9c78fb8af08763bbf53400d1254146ef
patri-create <59368869+patri-create@users.noreply.github.com>
2021-02-23 17:34:24 +0100

    feature: admin controller

--- clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/controllers/AdminController.java
+++ clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/controllers/AdminController.java
@@ -3,10 +3,14 @@ package es.urjc.etsii.co.clickandbuyweb.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import es.urjc.etsii.co.clickandbuyweb.models.Administrator;
+import es.urjc.etsii.co.clickandbuyweb.models.User;
 import es.urjc.etsii.co.clickandbuyweb.service.AdministratorService;
 
 @RestController
@@ -19,5 +23,30 @@ public class AdminController {
 	public List<Administrator> getAdministrators(){
 		return adminservice.getAdministrators();
 	}
-
+	
+	@PostMapping("/add")
+	public String newAdmin(@RequestParam(required=true) String email, @RequestParam(required=true) String password, @RequestParam(required=true) String charge) {
+		return adminservice.newAdmin(email, password, charge);
+	}
+	
+	@DeleteMapping("/delete")
+	public String deleteAdminId(@RequestParam(required=true) int id) {
+		return adminservice.deleteAdminId(id);
+	}
+	
+	@GetMapping("/search")
+	public Administrator adminEmailSearch(@RequestParam(required=true) String email) {
+		return adminservice.adminEmailSearch(email);
+	}
+	
+	@PostMapping("/changePassword")
+	public String changeAdminPassword(@RequestParam(required=true) String email, @RequestParam(required=true) String password, @RequestParam(required=true) String newPassword) {
+		return adminservice.adminChangePassword(email,password,newPassword);
+	}
+	
+	@PostMapping("/changeName")
+	public String changeAdminName(@RequestParam(required=true) String email, @RequestParam(required=true) String password, @RequestParam(required=true) String newName) {
+		return adminservice.adminChangeName(email,password,newName);
+	}
+	
 }
