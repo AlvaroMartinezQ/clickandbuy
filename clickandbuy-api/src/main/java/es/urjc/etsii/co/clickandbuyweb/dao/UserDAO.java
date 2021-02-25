502cc92911c5ed415223c895ef3e23d6d00b635d
Alvaro Martinez <alvaroo2302@gmail.com>
2021-02-25 18:32:23 +0100

    feature: custom querys for userDAO

--- clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/dao/UserDAO.java
+++ clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/dao/UserDAO.java
@@ -1,7 +1,10 @@
 package es.urjc.etsii.co.clickandbuyweb.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import es.urjc.etsii.co.clickandbuyweb.models.User;
@@ -11,5 +14,11 @@ public interface UserDAO extends JpaRepository<User, Integer>{
 	
 	@Query("SELECT u FROM User u WHERE u.user_email=:email")
 	public User findByUser_email(String email);
+	
+	@Query("SELECT u FROM User u WHERE u.user_name LIKE %:name%")
+	public List<User> searchByUser_name(@Param("name") String name);
+	
+	@Query("SELECT u FROM User u WHERE u.id=:id")
+	public User findByUser_id(int id);
 	
 }
