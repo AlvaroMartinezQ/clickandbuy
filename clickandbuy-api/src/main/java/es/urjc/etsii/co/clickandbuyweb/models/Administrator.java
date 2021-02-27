16ee56670acd9d87acca84d87d3e875b90e30525
patri-create <59368869+patri-create@users.noreply.github.com>
2021-02-27 13:45:54 +0100

    feature: add security in model admin

--- clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/models/Administrator.java
+++ clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/models/Administrator.java
@@ -80,6 +80,8 @@ public class Administrator implements Serializable {
 	}
 
 	public void setAdmin_name(String admin_name) {
+		if(admin_name.isBlank())
+			admin_name = "defaultName";
 		this.admin_name = admin_name;
 	}
 
@@ -96,6 +98,8 @@ public class Administrator implements Serializable {
 	}
 
 	public void setAdmin_phone(int admin_phone) {
+		if(admin_phone == 0)
+			admin_phone = 000000000;
 		this.admin_phone = admin_phone;
 	}
 
