07111edcdc2837b89fe70eeae44af6ebe7a58649
Alvaro Martinez <alvaroo2302@gmail.com>
2021-02-28 23:35:01 +0100

    feature: user photo services

---
+++ clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/service/UserImageService.java
@@ -0,0 +1,53 @@
+package es.urjc.etsii.co.clickandbuyweb.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import es.urjc.etsii.co.clickandbuyweb.dao.UserDAO;
+import es.urjc.etsii.co.clickandbuyweb.dao.UserImageDAO;
+import es.urjc.etsii.co.clickandbuyweb.models.User;
+import es.urjc.etsii.co.clickandbuyweb.models.UserImage;
+
+@Service
+public class UserImageService {
+
+	@Autowired
+	private UserImageDAO userimagedao;
+	@Autowired
+	private UserDAO userdao;
+	
+	public int upload(MultipartFile image, int usid) throws Exception {
+		User u = userdao.findByUser_id(usid);
+		if(u==null) {
+			// Error code for a non existing user
+			return -1;
+		}
+		if(usid<=0) {
+			// error code for a non valid user id
+			return 0;
+		}
+		UserImage existingImage = userimagedao.findByUser_id(usid);
+		if(existingImage==null) {
+			UserImage uploadImage = new UserImage();
+			uploadImage.setContent(image.getBytes());
+			uploadImage.setUsid(u.getId());
+			System.out.println("Non existing image");
+			return userimagedao.save(uploadImage).getId();
+		} else {
+			existingImage.setContent(image.getBytes());
+			System.out.println("Existing image");
+			return userimagedao.save(existingImage).getId();
+		}
+	}
+	
+	public Resource getImage(int usid) {
+		byte[] image = userimagedao.findbyid(usid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)).getContent();
+		return new ByteArrayResource(image);
+	}
+	
+}
