6057078b604ed588037c53147a9a1c95ebafbaeb
patri-create <59368869+patri-create@users.noreply.github.com>
2021-02-28 14:00:47 +0100

    feature: adding rating endpoints

--- clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/controllers/RatingController.java
+++ clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/controllers/RatingController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.urjc.etsii.co.clickandbuyweb.models.Product;
 import es.urjc.etsii.co.clickandbuyweb.models.Rating;
+import es.urjc.etsii.co.clickandbuyweb.models.User;
 import es.urjc.etsii.co.clickandbuyweb.service.RatingService;
 
 @RestController
@@ -19,6 +23,11 @@ public class RatingController {
 	@GetMapping("/all")
 	public List<Rating> getRating() {
 		return ratingservice.getRatings();
+	}
+	
+	@PostMapping("/add")
+	public String newRating(@RequestParam(required=true) String comment, @RequestParam(required=true) int rate, @RequestParam(required=true) int idUser, @RequestParam(required=true) int idProduct) {
+		return ratingservice.newRating(comment, rate, idUser, idProduct);
 	}
 
 }
