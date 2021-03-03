09438eb7097350f3558a3fdd969ff2d80d3b4959
lblay <l.blay.2017@alumnos.urjc.es>
2021-03-03 22:04:51 +0100

    feature: new services for orders

--- clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/service/OrderService.java
+++ clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/service/OrderService.java
@@ -1,5 +1,8 @@
 package es.urjc.etsii.co.clickandbuyweb.service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -14,35 +17,37 @@ import es.urjc.etsii.co.clickandbuyweb.models.User;
 
 @Service
 public class OrderService {
-	
+
 	@Autowired
 	private OrderDAO orderdao;
 	@Autowired
 	private UserDAO userdao;
 	@Autowired
 	private ProductDAO productdao;
-	
+
+	String[] orderstatus = { "POR APROBAR", "EN PREPARACION", "EN REPARTO", "ENTREGADO", "CANCELADO" };
+
 	public List<Order> getOrders() {
 		return orderdao.findAll();
 	}
-	
-	public List<Order> getOrder(int order_id){
+
+	public List<Order> getOrder(int order_id) {
 		return orderdao.findbyorder_id(order_id);
 	}
-	
+
 	public Order makeOrder(int usid, int prodid, int orderid) {
 		// Search if the given user and product exist
 		User u = userdao.findById(usid).orElseThrow();
 		Product p = productdao.findById(prodid).orElseThrow();
-		if(orderid==0) {
+		if (orderid == 0) {
 			// New order
 			Order neworder = new Order();
-			Date d =  new Date();
+			Date d = new Date();
 			neworder.setDate(d);
 			List<Order> orderlist = orderdao.findAll();
 			int id = 0;
-			for(Order o: orderlist) {
-				if(o.getOrder_id()>id) {
+			for (Order o : orderlist) {
+				if (o.getOrder_id() > id) {
 					id = o.getOrder_id();
 				}
 			}
@@ -56,7 +61,7 @@ public class OrderService {
 		} else {
 			// Existing order
 			Order existingorder = new Order();
-			Date d =  new Date();
+			Date d = new Date();
 			existingorder.setDate(d);
 			existingorder.setOrder_id(orderid);
 			existingorder.setOwner(u);
@@ -66,4 +71,50 @@ public class OrderService {
 		}
 	}
 
+	public String changeStatus(int order_id, int status_id) {
+		List<Order> newlist = orderdao.findbyorder_id(order_id);
+		if (newlist.isEmpty())
+			return "status: order not found";
+		;
+		Order o = newlist.get(0);
+		o.setState(orderstatus[status_id]);
+		if (status_id == 1) {
+			Date d = o.getDate();
+			Calendar c = Calendar.getInstance();
+			c.setTime(d);
+			c.add(Calendar.DATE, (int) (Math.random() * 7 + 2));
+			d = c.getTime();
+			o.setEstimated_date(d);
+		}
+		orderdao.save(o);
+		return "status: order status update";
+	}
+
+	public String deleteOrder(int order_id) {
+		List<Order> list = orderdao.findbyorder_id(order_id);
+		if (list.isEmpty())
+			return "status: order not found";
+		for (Order o : list) {
+			orderdao.delete(o);
+		}
+		return "status: the order was removed";
+	}
+
+	public String orderStatus(int order_id) {
+		List<Order> list = orderdao.findbyorder_id(order_id);
+		if (list.isEmpty())
+			return "status: order not found";
+		return list.get(0).getState();
+	}
+
+	public String priceOrder(int order_id) {
+		List<Order> list = orderdao.findbyorder_id(order_id);
+		if (list.isEmpty())
+			return "status: order not found";
+		double finalprice = 0;
+		for (Order o : list) {
+			finalprice += o.getProduct().getProduct_price();
+		}
+		return Double.toString(finalprice);
+	}
 }
