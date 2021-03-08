d58a3c58917da8b589a458bffc289fd576f1091b
Alvaro Martinez <alvaroo2302@gmail.com>
2021-03-08 17:33:48 +0100

    unused imports

--- clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/service/OrderService.java
+++ clickandbuy-api/src/main/java/es/urjc/etsii/co/clickandbuyweb/service/OrderService.java
@@ -1,7 +1,5 @@
 package es.urjc.etsii.co.clickandbuyweb.service;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
