109e278a6f62dc76fd2178ec9d984fd0fe9c8cb2
patri-create <59368869+patri-create@users.noreply.github.com>
2021-03-23 13:43:18 +0100

    feature: updating model admin

--- clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/models/Admin.java
+++ clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/models/Admin.java
@@ -1,17 +1,27 @@
 package es.urjc.etsii.co.clickandbuyweb.models;
 
+import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.web.context.annotation.SessionScope;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
+@SessionScope
 @Table(name="core_admin")
-public class Admin {
+public class Admin implements Serializable{
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
@@ -25,19 +35,22 @@ public class Admin {
 	private String realname;
 	@Column(nullable=false, length=12)
 	private String phone;
-	private Date last_login;
-	// Charge should be: [SUPERUSER, ADMIN, STAFF]
-	@Column(nullable=false, length=255)
-	private String admin_charge;
-	private boolean active;
-	private boolean superuser;
+    @JsonFormat(pattern="dd/MM/yyyy")
+    private LocalDate last_login;
+	// Charge should be: [MANAGER, ADMIN]
+	@Column(insertable=false, updatable=false)
+	private String charge;
+	
+	@ElementCollection(fetch=FetchType.EAGER)
+	private List<String> roles;
 	
 	public Admin() {
 		super();
 	}
 
-	public Admin(int id, String email, String password, String name, String realname,
-			String phone, Date last_login, String admin_charge, boolean is_active, boolean is_superuser) {
+
+	public Admin(int id, String email, String password, String name, String realname, String phone,
+			LocalDate last_login, String charge, List<String> roles) {
 		super();
 		this.id = id;
 		this.email = email;
@@ -46,96 +59,109 @@ public class Admin {
 		this.realname = realname;
 		this.phone = phone;
 		this.last_login = last_login;
-		this.admin_charge = admin_charge;
-		this.active = is_active;
-		this.superuser = is_superuser;
+		this.charge = charge;
+		this.roles = roles;
 	}
+
 
 	public int getId() {
 		return id;
 	}
 
+
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	public String getemail() {
+
+	public String getEmail() {
 		return email;
 	}
 
-	public void setemail(String email) {
+
+	public void setEmail(String email) {
 		this.email = email;
 	}
 
-	public String getpassword() {
+
+	public String getPassword() {
 		return password;
 	}
 
-	public void setpassword(String password) {
+
+	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	public String getname() {
+
+	public String getName() {
 		return name;
 	}
 
-	public void setname(String name) {
+
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getrealname() {
+
+	public String getRealname() {
 		return realname;
 	}
 
-	public void setrealname(String realname) {
+
+	public void setRealname(String realname) {
 		this.realname = realname;
 	}
 
-	public String getphone() {
+
+	public String getPhone() {
 		return phone;
 	}
 
-	public void setphone(String phone) {
+
+	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-	public Date getLast_login() {
+
+	public LocalDate getLast_login() {
 		return last_login;
 	}
 
-	public void setLast_login(Date last_login) {
+
+	public void setLast_login(LocalDate last_login) {
 		this.last_login = last_login;
 	}
 
-	public String getAdmin_charge() {
-		return admin_charge;
+
+	public String getCharge() {
+		return charge;
 	}
 
-	public void setAdmin_charge(String admin_charge) {
-		this.admin_charge = admin_charge;
+
+	public void setCharge(String charge) {
+		this.charge = charge;
 	}
 
-	public boolean isActive() {
-		return active;
+
+	public List<String> getRoles() {
+		return roles;
 	}
 
-	public void setActive(boolean is_active) {
-		this.active = is_active;
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
 	}
 
-	public boolean isSuperuser() {
-		return superuser;
-	}
-
-	public void setSuperuser(boolean is_superuser) {
-		this.superuser = is_superuser;
-	}
 
 	@Override
 	public String toString() {
-		return "Administrator [id=" + id + ", email=" + email + ", name=" + name
-				+ ", realname=" + realname + ", phone=" + phone + ", last_login=" + last_login
-				+ ", admin_charge=" + admin_charge + ", is_active=" + active + ", is_superuser=" + superuser
-				+ "]";
+		return "Admin [id=" + id + ", email=" + email + ", password=" + password + ", name=" + name + ", realname="
+				+ realname + ", phone=" + phone + ", last_login=" + last_login + ", charge=" + charge + ", roles="
+				+ roles + "]";
 	}
+
+	
 }
+
+	
