eb1e773ec2c9244ff4b48417d92fead389ea1790
lblay <l.blay.2017@alumnos.urjc.es>
2021-03-25 22:05:36 +0100

    feature: news controllers for productController

--- clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/controllers/ProductController.java
+++ clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/controllers/ProductController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,13 +28,48 @@ public class ProductController {
 	@RequestMapping("/productmain")
 	public ModelAndView singUp(Model model, HttpServletRequest request) {
 		Principal principal = request.getUserPrincipal();
-		if (principal != null) {
-			User u = us.getUser(principal.getName());
-			model.addAttribute("mail", u.getEmail());
-			model.addAttribute("userid", u.getId());
-			model.addAttribute("user", u);
-			return new ModelAndView("/product/productmain");
-		}
-		return new ModelAndView("user/singUp");
+
+		User u = us.getUser(principal.getName());
+		model.addAttribute("mail", u.getEmail());
+		model.addAttribute("userid", u.getId());
+		model.addAttribute("user", u);
+		return new ModelAndView("/product/productmain");
+
+	}
+
+	@RequestMapping("/upload")
+	public ModelAndView upload(Model model, HttpServletRequest request) {
+		Principal principal = request.getUserPrincipal();
+		User u = us.getUser(principal.getName());
+		model.addAttribute("mail", u.getEmail());
+		model.addAttribute("userid", u.getId());
+		model.addAttribute("user", u);
+		return new ModelAndView("/product/upload");
+	}
+
+	@RequestMapping("/uploadform")
+	public ModelAndView uploadForm(Model model, HttpServletRequest request, @RequestParam(required = true) String name,
+			@RequestParam(required = true) String desc, @RequestParam(required = true) Double price,
+			@RequestParam(required = true) int stock) {
+
+		Principal principal = request.getUserPrincipal();
+		User u = us.getUser(principal.getName());
+		productservice.saveProduct(u.getEmail(), price, name, desc, stock);
+		model.addAttribute("updated", true);
+		model.addAttribute("mail", u.getEmail());
+		model.addAttribute("userid", u.getId());
+		model.addAttribute("user", u);
+		return new ModelAndView("/product/upload");
+	}
+	
+	@RequestMapping("/owner")
+	public ModelAndView owner(Model model, HttpServletRequest request) {
+		Principal principal = request.getUserPrincipal();
+		User u = us.getUser(principal.getName());
+		model.addAttribute("products",u.getUser_product_list());
+		model.addAttribute("mail", u.getEmail());
+		model.addAttribute("userid", u.getId());
+		model.addAttribute("user", u);
+		return new ModelAndView("/product/owner");
 	}
 }
