d8b93c90651bdf77a825ad5da6938c1842666167
lblay <l.blay.2017@alumnos.urjc.es>
2021-03-27 12:32:26 +0100

    feature: new functionality

--- clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/controllers/ProductController.java
+++ clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/controllers/ProductController.java
@@ -61,15 +61,62 @@ public class ProductController {
 		model.addAttribute("user", u);
 		return new ModelAndView("/product/upload");
 	}
-	
+
 	@RequestMapping("/owner")
 	public ModelAndView owner(Model model, HttpServletRequest request) {
 		Principal principal = request.getUserPrincipal();
 		User u = us.getUser(principal.getName());
-		model.addAttribute("products",u.getUser_product_list());
+		model.addAttribute("products", u.getUser_product_list());
 		model.addAttribute("mail", u.getEmail());
 		model.addAttribute("userid", u.getId());
 		model.addAttribute("user", u);
-		return new ModelAndView("/product/owner");
+		return new ModelAndView("/product/list");
+	}
+
+	@RequestMapping("/search")
+	public ModelAndView search(Model model, HttpServletRequest request) {
+		Principal principal = request.getUserPrincipal();
+		User u = us.getUser(principal.getName());
+		model.addAttribute("mail", u.getEmail());
+		model.addAttribute("userid", u.getId());
+		model.addAttribute("user", u);
+		return new ModelAndView("/product/search");
+	}
+
+	@RequestMapping("/searchdata")
+	public ModelAndView searchData(Model model, HttpServletRequest request,
+			@RequestParam(required = true) String name) {
+		Principal principal = request.getUserPrincipal();
+		User u = us.getUser(principal.getName());
+		model.addAttribute("products", productservice.search(name));
+		model.addAttribute("mail", u.getEmail());
+		model.addAttribute("userid", u.getId());
+		model.addAttribute("user", u);
+		return new ModelAndView("/product/list");
+	}
+
+	@RequestMapping("/modify")
+	public ModelAndView update(Model model, HttpServletRequest request) {
+		Principal principal = request.getUserPrincipal();
+		User u = us.getUser(principal.getName());
+		model.addAttribute("mail", u.getEmail());
+		model.addAttribute("userid", u.getId());
+		model.addAttribute("user", u);
+		return new ModelAndView("/product/modify");
+	}
+
+	@RequestMapping("/modifyok")
+	public ModelAndView updateOk(Model model, HttpServletRequest request, @RequestParam(required = true) String id,
+			@RequestParam(required = true) String name, @RequestParam(required = true) String desc,
+			@RequestParam(required = true) String price, @RequestParam(required = true) String stock,
+			@RequestParam(required = true) boolean active) {
+		Principal principal = request.getUserPrincipal();
+		User u = us.getUser(principal.getName());
+		model.addAttribute("mail", u.getEmail());
+		model.addAttribute("userid", u.getId());
+		model.addAttribute("user", u);
+		model.addAttribute("updated", true);
+		model.addAttribute("result", productservice.updateProduct(id, name, desc, price, stock, active));
+		return new ModelAndView("/product/modify");
 	}
 }
