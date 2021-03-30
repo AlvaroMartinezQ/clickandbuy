c4d3cf7884ef368af36e2169b15929b63b01aded
lblay <l.blay.2017@alumnos.urjc.es>
2021-03-30 19:45:09 +0200

    feature: update orderservice

--- clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/service/OrderService.java
+++ clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/service/OrderService.java
@@ -1,5 +1,6 @@
 package es.urjc.etsii.co.clickandbuyweb.service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -15,28 +16,25 @@ import es.urjc.etsii.co.clickandbuyweb.dao.ProductDAO;
 import es.urjc.etsii.co.clickandbuyweb.dao.UserDAO;
 import es.urjc.etsii.co.clickandbuyweb.models.Cart;
 import es.urjc.etsii.co.clickandbuyweb.models.Order;
-import es.urjc.etsii.co.clickandbuyweb.models.Product;
 import es.urjc.etsii.co.clickandbuyweb.models.User;
 
 @Service
 @Transactional
 public class OrderService {
-	
+
 	@Autowired
 	private OrderDAO orderdao;
-	
+
 	@Autowired
 	private UserDAO userdao;
-	
+
 	@Autowired
 	private CartDAO cartdao;
-	
+
 	@Autowired
 	private ProductDAO productdao;
-	
-	String[] orderstatus = { "POR APROBAR", "EN PREPARACION", "EN REPARTO", "ENTREGADO", "CANCELADO" };
-	
-	public List<Order> getOrdes(){
+
+	public List<Order> getOrders() {
 		return orderdao.findAll();
 	}
 
@@ -47,30 +45,39 @@ public class OrderService {
 		}
 		return order.get();
 	}
-	
-	public String makeOrder(int usid, Set<Cart> carts) {
-		Optional<User> user = userdao.findById(usid);
-		
-		return "";
+
+	public String buy(int id) {
+		Optional<User> user = userdao.findById(id);
+		Order order = new Order(LocalDate.now(), LocalDate.now().plusDays((long) ((long) 2 + Math.random() * 7)),
+				"COMPLETADO", user.get().getOrderactive().getCarts());
+		order.priceTotal();
+		for(Cart c : order.getCarts()) {
+			c.getProduct().getBuyers().add(id);
+			productdao.save(c.getProduct());
+		}
+		user.get().getMyOrders().add(order);
+		user.get().setOrderactive(new Order());
+		orderdao.save(order);
+		userdao.save(user.get());
+		return "order completed!";
 	}
-	
+
 	public String addCart(int id, Cart cart) {
-		Optional<Order> order = orderdao.findById(id);
-		if(order.isPresent()) {
-			order.get().getCarts().add(cart);
+		Optional<User> user = userdao.findById(id);
+
+		if (user.isPresent()) {
+			user.get().getOrderactive().getCarts().add(cart);
+			cartdao.save(cart);
+			userdao.save(user.get());
 			return "added product";
 		}
 		return "product not added";
 	}
-	public String deleteCart(int id, Cart cart) {
-		Optional<Order> order = orderdao.findById(id);
-		if(order.isPresent()) {
-			order.get().getCarts().remove(cart);
-			return "delete product";
-		}
-		return "product not delete";
-	}
-	
-	
-	
+
+	/*
+	 * public String deleteCart(int id, Cart cart) {
+	 * 
+	 * }
+	 */
+
 }
