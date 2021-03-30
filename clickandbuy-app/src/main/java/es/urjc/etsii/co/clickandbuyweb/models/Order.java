513dbced9165bf0c68a5233c184ca7edc889383e
lblay <l.blay.2017@alumnos.urjc.es>
2021-03-30 19:44:18 +0200

    feature: update model Order

--- clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/models/Order.java
+++ clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/models/Order.java
@@ -1,6 +1,7 @@
 package es.urjc.etsii.co.clickandbuyweb.models;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -13,36 +14,27 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-
 @Entity
-@Table(name="marketplace_order")
+@Table(name = "marketplace_order")
 public class Order {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
 	private LocalDate date;
 	private LocalDate estimated;
 	private String state;
-	
+	private double price;
+
 	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name="cart_id")
-	private Set<Cart> carts;
-	
+	@JoinColumn(name = "cart_id")
+	private Set<Cart> carts = new HashSet<>();
+
 	public Order() {
 		super();
 	}
-	
-	public Order(int id, LocalDate date, LocalDate estimated, String state, Set<Cart> carts) {
-		super();
-		this.id = id;
-		this.date = date;
-		this.estimated = estimated;
-		this.state = state;
-		this.carts = carts;
-	}
-	
+
 	public Order(LocalDate date, LocalDate estimated, String state, Set<Cart> carts) {
 		super();
 		this.date = date;
@@ -91,13 +83,26 @@ public class Order {
 		this.carts = carts;
 	}
 
+	
+	public double getPriceTotal() {
+		return price;
+	}
+
+	public void setPriceTotal(double price) {
+		this.price = price;
+	}
+
+	public void priceTotal() {
+		this.price = 0;
+		for (Cart c : this.getCarts()) {
+			this.price += c.getPrice();
+		}
+	}
+
 	@Override
 	public String toString() {
 		return "Order [id=" + id + ", date=" + date + ", estimated=" + estimated + ", state=" + state + ", carts="
 				+ carts + "]";
 	}
-	
-	
-	
-	
+
 }
