35650f979866fb9e2cf352b02c4c3c4acb403e1f
lblay <l.blay.2017@alumnos.urjc.es>
2021-04-03 20:06:32 +0200

    feature: new controllers for ordercontroller

--- clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/controllers/OrderController.java
+++ clickandbuy-app/src/main/java/es/urjc/etsii/co/clickandbuyweb/controllers/OrderController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import es.urjc.etsii.co.clickandbuyweb.models.Order;
 import es.urjc.etsii.co.clickandbuyweb.models.User;
 import es.urjc.etsii.co.clickandbuyweb.service.OrderService;
 import es.urjc.etsii.co.clickandbuyweb.service.ProductService;
@@ -37,6 +38,37 @@ public class OrderController {
 			return new ModelAndView("/order/ordermain");
 		}
 		return new ModelAndView("user/singUp");
+	}
+	
+	@RequestMapping("/view")
+	public ModelAndView view(Model model, HttpServletRequest request) {
+		Principal principal = request.getUserPrincipal();
+		if (principal != null) {
+			User u = us.getUser(principal.getName());
+			model.addAttribute("orders",u.getMyOrders());
+			model.addAttribute("mail", u.getEmail());
+			model.addAttribute("userid", u.getId());
+			model.addAttribute("user", u);
+			
+			return new ModelAndView("/order/orderlist");
+		}
+		return new ModelAndView("/product/management");
+	}
+	
+	@RequestMapping("/seeorder")
+	public ModelAndView seeOrder(Model model, HttpServletRequest request, @RequestParam(required = true) int id) {
+		Principal principal = request.getUserPrincipal();
+		if (principal != null) {
+			User u = us.getUser(principal.getName());
+			Order order = orderservice.getOrderById(id);
+			model.addAttribute("order",order);
+			model.addAttribute("mail", u.getEmail());
+			model.addAttribute("userid", u.getId());
+			model.addAttribute("user", u);
+			
+			return new ModelAndView("/order/seeorder");
+		}
+		return new ModelAndView("/product/management");
 	}
 
 }
