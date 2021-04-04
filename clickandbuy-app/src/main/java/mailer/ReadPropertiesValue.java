09bc2c96bd27704f1b988ca7dee0f3d394ade4bd
Alvaro Martinez <alvaroo2302@gmail.com>
2021-04-04 20:35:49 +0200

    feature: load credentials from properties file

---
+++ clickandbuy-app/src/main/java/mailer/ReadPropertiesValue.java
@@ -0,0 +1,86 @@
+package mailer;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ReadPropertiesValue {
+
+	String result = "";
+	InputStream inputStream;
+	
+	public String getEmailHost() throws IOException {
+		String email = "";
+		try {
+			Properties prop = new Properties();
+			String propFileName = "config.properties";
+
+			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+			email = prop.getProperty("email_host");
+		} catch (Exception e) {
+			System.out.println("Exception while reading the email host");
+		} finally {
+			inputStream.close();
+		}
+		return email;
+	}
+	
+	public String getPasswordHost() throws IOException {
+		String password = "";
+		try {
+			Properties prop = new Properties();
+			String propFileName = "config.properties";
+
+			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+			password = prop.getProperty("email_password");
+		} catch (Exception e) {
+			System.out.println("Exception while reading the email host");
+		} finally {
+			inputStream.close();
+		}
+		return password;
+	}
+
+	// Get both host email and password
+	public String getPropValues() throws IOException {
+ 
+		try {
+			Properties prop = new Properties();
+			String propFileName = "config.properties";
+
+			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath.");
+			}
+
+			// get the property value and print it out
+			String email = prop.getProperty("email_host");
+			String pass = prop.getProperty("email_password");
+
+			result = email + " " + pass;
+			System.out.println(result);
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			inputStream.close();
+		}
+		return result;
+	}
+	
+}
