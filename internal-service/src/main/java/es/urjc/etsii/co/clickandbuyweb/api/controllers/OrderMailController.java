5f742cc3da13cc2b16707045caf03039be892393
lblay <l.blay.2017@alumnos.urjc.es>
2021-04-08 17:46:11 +0200

    feature: ordermailcontroller for internal service

---
+++ internal-service/src/main/java/es/urjc/etsii/co/clickandbuyweb/api/controllers/OrderMailController.java
@@ -0,0 +1,29 @@
+package es.urjc.etsii.co.clickandbuyweb.api.controllers;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import mailer.UserInfo;
+
+@RestController
+@RequestMapping("/ordermail")
+public class OrderMailController {
+	
+	@RequestMapping("/send")
+	public String send(@RequestParam(required=true)String data, @RequestParam(required=true)String email) {
+		System.out.println("\nRequest from main server. Payload: " + data + "\n");
+		UserInfo mail = new UserInfo(email, data);
+		/*
+		 * Next lines runs the email send in a different thread
+		 */
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+		executor.execute(() -> mail.setUp());
+		executor.shutdown();
+		return "status: sending user data email";
+	}
+
+}
